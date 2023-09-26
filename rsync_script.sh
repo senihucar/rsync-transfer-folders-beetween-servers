d134140fd4dd49195ba6c18766c3270198bf2616
Senih <40578755+senihucar@users.noreply.github.com>
2023-09-26 16:40:02 -0700

    Update rsync_script.sh

--- rsync_script.sh
+++ rsync_script.sh
@@ -1,86 +1,71 @@
 #!/bin/bash
-#Date
-TODAY=`date +%Y%m%d`
 
-#Time
-TS=`date +%H%M%S`
+#MAIL LIST
+MAIL="senih.ucar@yahoo.com"
 
-MAIL=senihucar@yahoo.com
+# Rsync SETTINGS
+BCKP_OPTS="-avzipogt"
 
-# directory to backup 1st
-BDIR1=/home/folder1
-# directory to backup 2st
-BDIR2=/home/folder2
+# Directories to backup (add more directories as needed)
+BACKUP_DIRS=(
+  "/home/sucar2/test5.dat"
+  "/home/sucar2/test10.dat"
+  "/home/sucar2/test24.dat"
+)
 
-# directory to destionation
-DDIR="/backup"
+#  destination directory on target machine
+DDIR="/home/sucar2"
 
-# the name of the destionation machine
-BSERVER=linux2
-
-# your password on the destionation server use SHA crypted
-#export RSYNC_PASSWORD=XXXXXX
-
-#HOSTNAME (Script run on this host)
-hs=`hostname`
-
-#rsync SETTINGS
-
-BCKP_OPTS="--archive --verbose --info=COPY2,DEL2,NAME2,BACKUP2,REMOVE2,SKIP2"
-
-#LOG file Email Settings
-
-mkdir $DDIR/log
-LOGFILE=$DDIR/log/$TODAY-$TS-sync.log
-echo $LOGFILE
-
-echo -e "Transfer Date: `date` - $hs>$BSERVER " >> $LOGFILE
-
-###########################################################################################
-#Level 1 if begin
-if [[ -e $BDIR1 && -e $BDIR2 ]]
-then
-  echo -e "The Backup folders is ready for transfer. && (Return code: $BDIR1) && (Return code: $BDIR2)" >> $LOGFILE
- #LS DIRECTORY 1
-  echo -e "Directory $BDIR1 exists." >> $LOGFILE
-  echo -e "The files on first directory to be transferred are listed below:"
-  ls -al $BDIR1 >> $LOGFILE
- #LS DIRECTORY 2
-  echo "Directory $BDIR2 exists." >> $LOGFILE
-  echo -e "The files on second directory to be transferred are listed below:"
-  ls -al $BDIR2 >> $LOGFILE
+# The name of the destination machine
+BSERVER="destination@host"
 
 
 
-############################################################################
-  bdir1log=$DDIR/log/$TODAY-$TS-rsynctransferbackup1log.txt
-  bdir2log=$DDIR/log/$TODAY-$TS-rsyncTransferbackup2log.txt
 
-  #### rsync ! Doing transfer and archive the file
-  rsync $BCKP_OPTS $BDIR1 $DDIR > $bdir1log
-  rsync_status1=$?
+### ***PLEASE DO NOT CHANGE BELOW*** ###
+# Date
+TODAY=$(date +%Y%m%d)
 
-  rsync $BCKP_OPTS $BDIR2 $DDIR > $bdir2log
-  rsync_status2=$?
-############################################################################
+# Time
+TS=$(date +%H%M%S)
 
-  #Level 2 if begin
-  if [[ $rsync_status1 = 0 && $rsync_status2 = 0 ]]; then
 
-    echo "The file transfer completed successfully. (Return code:$rsync_status1) (Return code:$rsync_status2)" >> $LOGFILE
-    echo `date` >> $LOGFILE
-    echo "The file transfer completed successfully. Please, check the attached log or $LOGFILE" |mailx -s "THE FILE TRANSFER SUCCESS! - $hs>$BSERVER" -a $LOGFILE -a $bdir1log -a $bdir2log $MAIL
+# HOSTNAME (Script run on this host)
+hs=$(hostname)
+
+# LOG file Email Settings
+mkdir -p "$PWD/log"
+LOGFILE="$PWD/log/$TODAY-$TS-sync.log"
+echo "$LOGFILE"
+
+echo -e "Transfer Date: $(date) - $hs > $BSERVER" >> "$LOGFILE"
+
+# Iterate over backup directories
+for BDIR in "${BACKUP_DIRS[@]}"; do
+  if [ -e "$BDIR" ]; then
+    echo "Directory $BDIR exists." >> "$LOGFILE"
+    echo "The files in the directory to be transferred are listed below:" >> "$LOGFILE"
+    ls -al "$BDIR" >> "$LOGFILE"
+
+    bdirlog="$PWD/log/$TODAY-$TS-$(basename "$BDIR")-rsync-transfer-log.txt"
+
+    # rsync! Doing transfer and archive the file
+    rsync "$BCKP_OPTS" "$BDIR" "$BSERVER:$DDIR" > "$bdirlog"
+    rsync_status=$?
+
+    if [ $rsync_status -eq 0 ]; then
+      echo "The file transfer for $BDIR completed successfully. (Return code: $rsync_status)" >> "$LOGFILE"
+      echo "$(date)" >> "$LOGFILE"
+      echo "The file transfer completed successfully. Please check the attached log: $bdirlog" | mail -s "FILE TRANSFER SUCCESS - $hs > $BSERVER" $MAIL < "$bdirlog"
+    else
+      echo "The file transfer for $BDIR failed. (Error code: $rsync_status)" >> "$LOGFILE"
+      echo "Notify the user to verify the log" >> "$LOGFILE"
+      echo "The file transfer for $BDIR failed. Please check the attached log: $bdirlog" | mail -s "FILE TRANSFER FAILED - $hs > $BSERVER" $MAIL < "$bdirlog"
+    fi
   else
-    echo "The file transfer failed. (Error code:$rsync_status1) (Error code:$rsync_status2)" >> $LOGFILE
-    echo "Notify the user to verify the the log" >> $LOGFILE \
-    echo "The file transfer failed. Please, check the attached log or  $LOGFILE" |mailx -s "THE FILE TRANSFER FAILED - $hs>$BSERVER" -a $LOGFILE -a $bdir1log -a $bdir2log $MAIL
+    echo "The file transfer for $BDIR failed. The backup transfer file is not ready. (Return code: $BDIR)" >> "$LOGFILE"
+    echo "Waiting for the system to create the backup transfer file" >> "$LOGFILE"
+    echo "$(date)" >> "$LOGFILE"
+    echo "The file transfer for $BDIR failed. The backup file is not ready. Unsuccessful." | mail -s "FILE TRANSFER FAILED - $hs > $BSERVER" $MAIL < "$LOGFILE"
   fi
-  #Level 2 if end
-
-else
-  echo "The file transfer failed. The backup transfer file is not ready. && (Return code: $BDIR1) && (Return code: $BDIR2)" >> $LOGFILE
-  echo "Waiting the system to create the backup transfer file" >> $LOGFILE
-  echo `date`  >> $LOGFILE
-  echo "The file transfer failed. The backup file is not ready. Unsuccesfull " | mailx -s "THE FILE TRANSFER FAILED - $hs>$BSERVER" -a $LOGFILE $MAIL
-fi
-#Level 1 if end
+done
